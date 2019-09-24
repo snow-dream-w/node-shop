9253015ce6f404d4a1a72a06fae407e0f36e39d8
beautiful-night <879223580@qq.com>
2019-09-24 09:18:52 +0800

    上传商品路由

---
+++ src/Routers/GoodsRouter.js
@@ -0,0 +1,10 @@
+const Router = require('koa-router')
+
+const user = require('../Controller/GoodsController')
+
+const router = new Router
+
+//商品上架
+router.post("/goods/shelf",user.addGoods);
+
+module.exports = router
