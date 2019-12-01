4b9a8c0396a9596da24d1d99ff4eb8d88baa3fd5
beautiful-night <879223580@qq.com>
2019-12-02 00:34:33 +0800

    推荐算法修改

--- src/Service/RecommendService.js
+++ src/Service/RecommendService.js
@@ -1,7 +1,7 @@
 const _ = require('lodash')
 module.exports = class RecommendService {
     /**
-     * //5ddaac06f82d1624e7faa151   推荐的商品ID
+     * 构造方法
      * @param {*倒查表所有数据组成的数组} data 
      * @param {*用户ID} userId 
      * @param {*相似度最高的前n个} n 
@@ -22,9 +22,15 @@ module.exports = class RecommendService {
         this.similarityList = []
         // 记录用户相似度前n个用户中所有购买的商品与本用户不重复的商品
         this.targetGoods = []
+        // 记录用户对筛选出的各商品感兴趣程度
+        this.interestedGrade = [];
+        // 记录最后的返回结果
+        this.result = []
     }
+    /**
+     * 入口
+     */
     start() {
-        // console.log(this.data)
         // 计算相似度，得出本用户和其他所有用户的相似度分数
         this.getUserArray()
         this.similarityList.sort((a, b) => {
@@ -32,9 +38,57 @@ module.exports = class RecommendService {
         })
         // 计算目标商品
         this.getTargetGoods()
-        // 此时目标商品已存在this.targetGoods中
-        // console.log(this.targetGoods)
-        return this.targetGoods
+        // 此时目标商品已存在this.targetGoods中, 然后去重
+        this.targetGoods = [...new Set(this.targetGoods)]
+
+        // 计算用户对每个商品的感兴趣程度
+        for (let goodsId of this.targetGoods.values()) {
+            this.getInterestedGrade(goodsId)
+        }
+        // 计算最终商品列表并逆序排序
+        this.getFinalResult()
+        console.log(this.result)
+        return this.result
+    }
+    /**
+     * 计算最终商品列表并逆序排序
+     */
+    getFinalResult() {
+        this.interestedGrade.sort((a, b) => {
+            return b.grade - a.grade
+        })
+        for (let obj of this.interestedGrade.values()) {
+            this.result.push(obj.goodsId)
+        }
+    }
+    /**
+     * 计算用户对该商品的感兴趣程度
+     * @param {*商品ID} goodsId 
+     */
+    getInterestedGrade(goodsId) {
+        // 筛选出对商品goodsId用过行为的用户
+        let array = new Set()
+        for (let obj of this.data.values()) {
+            if (obj.goodsId === goodsId) {
+                array.add(obj.userId)
+            }
+        }
+        const users = [...array]
+        // 计算感兴趣程度
+        let grade = 0
+        for (let userId of users.values()) {
+            for (let i = 0; i < this.n; i++) {
+                if (userId === this.similarityList[i].userId) {
+                    const res = this.getUserSimilarity(userId, this.similarityList[i].userId)
+                    grade += res
+                }
+            }
+        }
+        // 添加到最终结果
+        this.interestedGrade.push({
+            goodsId: goodsId,
+            grade: grade
+        })
     }
     /**
      * 获取目标商品数组
@@ -44,11 +98,6 @@ module.exports = class RecommendService {
         // 截至目前，以获取用户相似度的逆序排序数组，以下为获取前n个相似用户购买的所有商品中本用户没买过的
         for (let index = 0; index < this.n; index++) {
             const element = this.similarityList[index]
-            // for (let obj of this.data.values()) {
-            //     if (obj.userId === element.userId) {
-            //         this.targetGoods.push(obj.goodsId)
-            //     }
-            // }
             _.filter(this.data, obj => {
                 if (obj.userId == element.userId) {
                     this.targetGoods.push(obj.goodsId)
@@ -90,15 +139,17 @@ module.exports = class RecommendService {
         this.userArray = [...(new Set(arrayTemp))]
         // 遍历计算与每个用户的相似度
         for (let index in this.userArray) {
-            this.getUserSimilarity(this.userArray[index])
+            this.getUserSimilarity(this.userId, this.userArray[index])
         }
     }
     /**
-     * 计算与其他用户的相似度
+     * 计算两个用户的相似度
+     * @param {*用户ID} userId 
+     * @param {*另一个用户ID} otherUserId 
      */
-    getUserSimilarity(otherUserId) {
+    getUserSimilarity(userId, otherUserId) {
         const userSelfGoods = _.filter(this.data, obj => {
-            return this.userId == obj.userId
+            return userId == obj.userId
         })
         this.filterUserGoods(otherUserId)
         // 计算相似度的分母
@@ -121,9 +172,11 @@ module.exports = class RecommendService {
             grade: this.top / this.bottom
         }
         this.similarityList.push(obj)
+        return obj.grade
     }
     /**
      * 过滤出用户otherUserId的商品列表
+     * @param {用户ID} otherUserId 
      */
     filterUserGoods(otherUserId) {
         this.userGoodsTemp = _.filter(this.data, obj => {
@@ -132,6 +185,7 @@ module.exports = class RecommendService {
     }
     /**
      * 过滤出商品goodsId的商品列表
+     * @param {商品ID} goodsId 
      */
     filterGoodsById(goodsId) {
         const goods = _.filter(this.data, obj => {
